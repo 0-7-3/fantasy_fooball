25a927ccc5d1ae00684869ba23b0cc82cf9a4f67
Rao Bilal <raobilal073@gmail.com>
2017-08-28 19:22:00 +0500

    added matches

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 	resources :admin, only: [:index]
 	namespace :admin do
 		resources :players
+		resources :teams
+		resources :games
+		resources :matches
 	end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
